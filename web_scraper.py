a7efb02ea489cabcd61367c0fc7299c3bd7d1521
Simon Koch <simon.leo.koch@tum.de>
2024-04-22 11:51:25 +0200

    Add scraping for multiple pages of events

--- web_scraper.py
+++ web_scraper.py
@@ -16,10 +16,10 @@ class Event:
     def __repr__(self) -> str:
         return f'{self.title}, {self.datetime}, {self.description}'
 
-events = set()
+found_events = set()
 
-url = 'https://hacker-school.de/unterstuetzen/inspirer/checkin-inspirer-yourschool/?formats%5B0%5D=ys'
-response = requests.get(url)
+base_url = 'https://hacker-school.de/unterstuetzen/inspirer/checkin-inspirer-yourschool/?formats%5B0%5D=ys'
+response = requests.get(base_url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
 def find_events_on_page(soup: BeautifulSoup) -> set:
@@ -33,6 +33,15 @@ def find_events_on_page(soup: BeautifulSoup) -> set:
         events.add(Event(title, date_and_time, course_description))
     return events
 
+pagination = soup.find('div', class_='hs-event-pagination')
+number_of_pages = int(pagination.find_all('li')[-2].text)
+
+for i in range(1, number_of_pages + 1):
+    page_url = base_url + f'&page={i}'
+    response = requests.get(page_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    found_events.update(find_events_on_page(soup))
+
 # Initialize known_events as an empty set
 known_events = set()
 
@@ -44,13 +53,11 @@ if os.path.exists(known_events_file_path):
     # If the file exists, load the known events from the file
     with open(known_events_file_path, 'rb') as file:
         known_events = pickle.load(file)
-found_events = set(find_events_on_page(soup))  # Convert the list of found events to a set
 
 new_events = found_events - known_events  # Find the difference between the found events and known events
 
 # Print the new events
 print('\n'.join([str(e) for e in new_events]))
 
-# Update known_events with the new events
-known_events.update(new_events)
+known_events = found_events
 
